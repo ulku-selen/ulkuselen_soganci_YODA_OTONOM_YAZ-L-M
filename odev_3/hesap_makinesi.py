70f6bb53f001e943c6e6c6bd77a5f58cd0ae7449
ulku-selen <selen19500@hotmail.com>
2025-11-27 11:23:17 +0300

    hesap_makinesi.py

--- odev_3/hesap_makinesi.py
+++ odev_3/hesap_makinesi.py
@@ -2,16 +2,10 @@ print("Basit Hesap Makinesi")
 print("İşlemler: +  -  *  /")
 while True:  
     islem = input("İşlem seçiniz (+, -, *, / ): ")
-
-    if islem not in ["+", "-", "*", "/"]:
-        print("Geçersiz işlem! Tekrar deneyin.")
-        continue
-
-    # Kullanıcıdan iki sayı alıyoruz
+    
     sayi1 = float(input("Birinci sayıyı giriniz: "))
     sayi2 = float(input("İkinci sayıyı giriniz: "))
 
-    # Her işlem türüne göre sonuç hesaplama
     if islem == "+":
         sonuc = sayi1 + sayi2
     elif islem == "-":
@@ -19,9 +13,9 @@ while True:
     elif islem == "*":
         sonuc = sayi1 * sayi2
     elif islem == "/":
-        # Sıfıra bölme kontrolü
+      
         if sayi2 == 0:
-            print("HATA: Bir sayı sıfıra bölünemez!")
+            print("error:sayı sıfıra bölünemez!")
             continue
         sonuc = sayi1 / sayi2
 
